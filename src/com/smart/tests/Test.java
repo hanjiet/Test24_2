db3f22eb8cbe8de8dd8d9f22f5bcb8c39f27c8a2
hanjiet <thj2010@gmail.com>
2024-01-06 12:24:51 -0500

    Second Commit.

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -5,5 +5,6 @@ public class Test {
 	@org.testng.annotations.Test
 	public void loginTest() {
 		System.out.println("We are learning Git and GitHub through Eclipse");
+		System.out.println("We had our initial commit");
 	}
 }
