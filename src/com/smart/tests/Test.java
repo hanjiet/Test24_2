ed4fe25cef7352cd3c6ece8b4b5346716f442596
hanjiet <119808511+hanjiet@users.noreply.github.com>
2024-01-06 13:03:03 -0500

    Update Test.java

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	
 	@org.testng.annotations.Test
 	public void loginTest() {
+		//Add comment. Can do changes by going to editor and then commit the changes
 		System.out.println("We are learning Git and GitHub through Eclipse");
 		System.out.println("We had our initial commit");
 		System.out.println("Third commit");
